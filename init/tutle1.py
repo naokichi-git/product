b23a8e0fa785dfb51b7c2bb9f34adad38fda66d9
NaokiHashizume <50830297+NaokiHashizume@users.noreply.github.com>
2020-05-03 19:24:14 +0900

    janken

--- init/tutle1.py
+++ init/tutle1.py
@@ -1,31 +1,33 @@
 from turtle import *
 shape("turtle")
+pensize(5)
+
 color("blue")
 circle(100)
 right(45)
 
-color("white")
+color("")
 forward(100)
 
 color("yellow")
 circle(100)
 left(63)
 
-color("white")
+color("")
 forward(250)
 
 color("black")
 circle(100)
 right(90)
 
-color("white")
+color("")
 forward(40)
 
 color("green")
 circle(100)
 left(80)
 
-color("white")
+color("")
 forward(250)
 
 color("red")
